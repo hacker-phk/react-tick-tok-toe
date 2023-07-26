a22fa454a6083a4aeb7ec30d5b814936caedd2c4
Hemanthkumar <107429016+hacker-phk@users.noreply.github.com>
2023-07-26 07:34:33 +0530

    Passing data through props

--- index.js
+++ index.js
@@ -4,5 +4,44 @@ import './styles.css';
 
 import App from './App';
 export default function Square() {
-  return <button className="square">X</button><button className="square">X</button>;
+  return  (
+    <>
+     <div className="board-row">
+        <button className="square">1</button>
+        <button className="square">2</button>
+        <button className="square">3</button>
+      </div>
+      <div className="board-row">
+        <button className="square">4</button>
+        <button className="square">5</button>
+        <button className="square">6</button>
+      </div>
+      <div className="board-row">
+        <button className="square">7</button>
+        <button className="square">8</button>
+        <button className="square">9</button>
+      </div>
+    </>
+  );
+}
+export default function Board() {
+  return (
+    <>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+    </>
+  );
 }
