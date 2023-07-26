2c2082b3faf101f1f1250f9a915cbf694628fd1c
Hemanthkumar <107429016+hacker-phk@users.noreply.github.com>
2023-07-26 07:31:57 +0530

    Building the board

---
+++ index.js
@@ -0,0 +1,8 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './styles.css';
+
+import App from './App';
+export default function Square() {
+  return <button className="square">X</button><button className="square">X</button>;
+}
